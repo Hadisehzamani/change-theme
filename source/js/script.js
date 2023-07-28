af77fd4ccfe4c7bfee6e24f99e6fb5e2b3754a24
Hadisehzamani <hadisehzmn@gmail.com>
2023-07-28 16:39:59 +0330

    set it in local storage

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,17 @@
-let btns = document.getElementsByClassName('btn')
-let themes = document.querySelector('.themes')
+let btns = document.getElementsByClassName('btn');
+let themes = document.querySelector('.themes');
 
-themes.addEventListener('click', function(e){
-    if(e.target.nodeName == 'BUTTON'){
-        let color = e.target.dataset.color
-        document.documentElement.style.setProperty('--theme-color', color)
+
+let storedColor = localStorage.getItem('themeColor');
+
+if (storedColor) {
+  document.documentElement.style.setProperty('--theme-color', storedColor);
+}
+
+themes.addEventListener('click', function(e) {
+    if (e.target.nodeName == 'BUTTON') {
+        let color = e.target.dataset.color;
+        document.documentElement.style.setProperty('--theme-color', color);
+        localStorage.setItem('themeColor', color);
     }
-})
+});
